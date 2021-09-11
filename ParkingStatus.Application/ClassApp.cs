bb42a4c6562ebd819c1e433b774e77dd1f8be446
dennalig <dennaligrissom7@gmail.com>
2021-09-11 00:29:43 -0400

    Add project files.

---
+++ ParkingStatus.Application/ClassApp.cs
@@ -0,0 +1,15 @@
+﻿using ParkingStatus.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingStatus.Application
+{
+    public class ClassApp
+    {
+
+        Class1 class1;
+    }
+}
