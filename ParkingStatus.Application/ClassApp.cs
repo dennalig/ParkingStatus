87705adfc50665bb0c9af888d803e5519e17d654
dennalig <dennaligrissom7@gmail.com>
2021-09-15 10:50:56 -0400

    "Add 4 Domain Model Classes"

--- ParkingStatus.Application/ClassApp.cs
+++ ParkingStatus.Application/ClassApp.cs
@@ -1,10 +1,10 @@
-﻿using ParkingStatus.Domain;
+﻿
 using System;
 
 namespace ParkingStatus.Application
 {
     public class ClassApp
     {
-        Class1 class1;
+     
     }
 }
--- ParkingStatus.Domain/ParkingLot.cs
+++ ParkingStatus.Domain/ParkingLot.cs
@@ -2,7 +2,7 @@
 
 namespace ParkingStatus.Domain
 {
-    public class Class1
+    public class ParkingLot
     {
     }
 }
