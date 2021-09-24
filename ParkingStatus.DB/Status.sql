ebd1d4cde6539af6e43bbc181f3d58141e54f3d2
dennalig <dennaligrissom7@gmail.com>
2021-09-24 10:23:32 -0400

    "Add database project and Status Table"

---
+++ ParkingStatus.DB/Status.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Status]
+(
+	[StatusID] INT NOT NULL PRIMARY KEY, 
+    [StatusName] VARCHAR(MAX) NOT NULL, 
+    [StatusColor] VARCHAR(50) NULL, 
+    [StatusDescription] VARCHAR(MAX) NULL, 
+    [StatusImage] IMAGE NULL
+)
