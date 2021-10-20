946d04f629e11b60c645dd155768e032124f5ad3
dennalig <dennaligrissom7@gmail.com>
2021-10-20 10:46:34 -0400

    Create Domain Repositories for Lots, Statuses, and StatusEvents

---
+++ ParkingStatus.Domain/Repository/IStatusEventRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingStatus.Domain.Repository
+{
+    public interface IStatusEventRepository
+    {
+        Task<IEnumerable<StatusEvent>> GetAllAsync();
+
+        Task<IEnumerable<StatusEvent>> GetAllOfStatus(Status status); // get all status Events of a certain status
+
+        Task<StatusEvent> GetByIdAsync(int statusEventId);
+
+        void AddStatusEvent(StatusEvent statusEvent);
+
+        void DeleteStatusEvent(StatusEvent statusEvent);
+
+        void EditStatusEvent(StatusEvent statusEvent); // in the edit, this is also where we would alter statusevent dates
+    }
+}
---
+++ ParkingStatus.Domain/Repository/IStatusRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingStatus.Domain.Repository
+{
+    public interface IStatusRepository
+    {
+        Task<IEnumerable<Status>> GetAllAsync();
+
+        Task<Status> GetByIdAsync(int statusId);
+
+        void AddStatus(Status status);
+
+        void DeleteStatus(Status status);
+
+        void EditLot(Status status);
+    }
+}
