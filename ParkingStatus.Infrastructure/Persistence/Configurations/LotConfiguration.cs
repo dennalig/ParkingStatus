a42e01a8e117244532ddcaa6e0dace46fc289d9a
dennalig <dennaligrissom7@gmail.com>
2021-10-23 16:48:26 -0400

    Change Solution/ project to .net 5, Create LotConfiguration in Infrastructure, Add Microsoft.EntityFrameworkCore.Relational Nuget package

---
+++ ParkingStatus.Infrastructure/Persistence/Configurations/LotConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ParkingStatus.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingStatus.Infrastructure.Persistence.Configurations
+{
+    internal sealed class LotConfiguration : IEntityTypeConfiguration<Lot>
+    {
+        public void Configure(EntityTypeBuilder<Lot> builder)
+        {
+            //https://stackoverflow.com/questions/43200184/entitytypebuilder-does-not-contain-a-definition-for-totable-in-ef-core
+            builder.ToTable(nameof(Lot));
+        }
+    }
+}
