8a53968a4667e25130612393223e7812202e48fa
dennalig <dennaligrissom7@gmail.com>
2021-10-24 00:04:16 -0400

    Web nuget packages

---
+++ ParkingStatus.Web/scripts/JS/App.jsx
@@ -0,0 +1,9 @@
+﻿var Hello = React.createClass({
+    render: function () {
+        return (
+            <div> Hello World</div>
+        );
+    }
+});
+
+React.render(<Hello />, document.getElementById("reactdiv"));
