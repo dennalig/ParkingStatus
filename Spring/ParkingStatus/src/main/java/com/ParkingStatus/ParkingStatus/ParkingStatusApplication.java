eaa00330daf05e7ffee2df2a4f066797ea5c4ba8
dennalig <dennaligrissom7@gmail.com>
2021-10-26 18:59:47 -0400

    Add Spring Project

---
+++ Spring/ParkingStatus/src/main/java/com/ParkingStatus/ParkingStatus/ParkingStatusApplication.java
@@ -0,0 +1,13 @@
+package com.ParkingStatus.ParkingStatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingStatusApplication.class, args);
+	}
+
+}
---
+++ Spring/ParkingStatus/src/test/java/com/ParkingStatus/ParkingStatus/ParkingStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ParkingStatus.ParkingStatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
