91a02180580120197fe3f89886dbf7b16d608001
dennalig <dennaligrissom7@gmail.com>
2021-10-31 00:32:46 -0400

    replace .js with .tsx files

---
+++ Spring/ParkingStatus/src/js/parkingstatus.web/src/App.tsx
@@ -0,0 +1,12 @@
+
+import './App.css';
+import NavigationBar from './main_components/NavigationBar';
+
+//https://create-react-app.dev/docs/adding-typescript/
+function App() {
+  return (
+    <NavigationBar />
+  );
+}
+
+export default App;
