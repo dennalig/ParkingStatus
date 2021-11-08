a9fa9e2ae85e4f8fbe6fe7a039da0a1380ec635d
dennalig <dennaligrissom7@gmail.com>
2021-11-08 10:50:44 -0500

    SignUp input with useState

--- Spring/ParkingStatus/src/js/parkingstatus.web/src/main_components/signup_page/SignUp.tsx
+++ Spring/ParkingStatus/src/js/parkingstatus.web/src/main_components/signup_page/SignUp.tsx
@@ -1,35 +1,67 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import '../general_style/sign_up_login_style.css';
 
-const SignUp: React.FC = () => {
-    return (
 
-        <div>
-            <div className='title_style'>
-                <h1> Sign Up </h1>
-            </div>
+type NewUser ={
+    new_email: string,
+    new_password: string
+}
 
-            <div className="input_style">
+export const SignUp = () => {
 
-                <label>Email:
-                <input id="newEmail" type="email" 
-                    placeholder="Enter an email here.">
-                </input>
+    const[user, setUser] = useState<NewUser|null>(null);
+
+    const handleEmailValidation = () => {
+        setUser({
+            new_email:'newemail@gmail.com',
+            new_password:'pw'
+        });
+    }
+    const handlePwValidation = () => {}
+
+    const handleNewAdminUserSubmission = () => {
+        console.log('here');
+        handleEmailValidation();
+    }
+
+        return (
+
+            <div>
+                <div className='title_style'>
+                    <h1> Sign Up </h1>
+                </div>
+    
+                <form>
+                <div className="input_style">
+    
+                    <label>Email:
+                    <input id="newEmail" type="email"
+                        placeholder="Enter an email here.">
+                    </input>
+                    </label>
+    
+                </div>
+    
+                <div className="input_style">
+                <label>Password:
+                <input id="newPassword" type="password" 
+                        placeholder="Enter a password here.">
+                    </input>
                 </label>
-
+                </div>
+    
+                <div className="button_style">
+                    <button onClick={handleNewAdminUserSubmission}>Submit</button>
+                </div>
+    
+    <div> {user?.new_email}</div>
+                </form>
+                
             </div>
-
-            <div className="input_style">
-            <label>Password:
-            <input id="newPassword" type="password" 
-                    placeholder="Enter a password here.">
-                </input>
-            </label>
-            </div>
-            
-        </div>
-            );
+                );
+    
+    
    
 }
 export default SignUp;
