e6f780d23381281d8e15623257174c52c3f49954
dennalig <dennaligrissom7@gmail.com>
2021-11-09 22:20:31 -0500

    create function signatures for AdminUserService class

--- Spring/ParkingStatus/src/js/parkingstatus.web/src/services/AdminUserService.ts
+++ Spring/ParkingStatus/src/js/parkingstatus.web/src/services/AdminUserService.ts
@@ -4,6 +4,25 @@ const ADMINUSERS_REST_API_URL : string = 'http://localhost:8080/api/v1/adminuser
 
 class AdminUserService{
 
+    getAdminUsers(){
+
+    }
+
+    getAdminUserOfEmail(inputEmail : string){
+
+    }
+
+    inputEmailIsValid(inputEmail: string){
+
+    return false;
+    }
+
+    inputPasswordIsValid(inputPassword: string){
+        return false;
+    }
+
+
+
     
 
 }
