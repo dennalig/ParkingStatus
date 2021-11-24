20be1aa607c4139d5dd1f5ea2bc1dac51c89af2e
dennalig <dennaligrissom7@gmail.com>
2021-11-24 12:07:03 -0500

    finish lot accordion condtional styling!

--- Spring/ParkingStatus/src/js/parkingstatus.web/src/main_components/front_page/AccordionEntry.tsx
+++ Spring/ParkingStatus/src/js/parkingstatus.web/src/main_components/front_page/AccordionEntry.tsx
@@ -5,6 +5,7 @@ import { CurrentParsedTimeContext } from './CurrentParsedTimeContext';
 import FrontPageHandler from '../../Utility/FrontPageUtility/FrontPageHandler';
 
 import './style/front_page_style.css';
+import './ColorLegend';
 
 
 //current props
@@ -20,11 +21,15 @@ const AccordionEntry: React.FC<any> = (props) => {
 
     const[isActive, setIsActive] = useState(false);
 
-    const [statusEventValue, setStatusEventValue] = useState<any>(null); // check status event state first 
+    const [statusEventValue, setStatusEventValue] = useState<Array<any>>([]); // check status event state first 
 
-    const[lssValue, setLssValue] = useState<any>(null);
+    const[lssValue, setLssValue] = useState<Array<any>>([]);
+
+    const[statusColor , setStatusColor] = useState<string>('grey');
 
     const currentDateTimeValue : Date = useContext(CurrentParsedTimeContext);
+
+    
 
     // console.log(currentDateTimeValue);
  
@@ -47,6 +52,32 @@ const AccordionEntry: React.FC<any> = (props) => {
     }, [props.statuses, props.statusEvents, props.timeZone, props.currentDate]);
 
     
+    useEffect(() =>{
+        if(statusEventValue !== null){
+            if(statusEventValue[2] !== null){
+                console.log(statusEventValue[2]?.color);
+                setStatusColor(statusEventValue[2]?.color);
+                //https://stackoverflow.com/questions/40735406/checking-for-undefined-in-react
+                //?. helps
+            }
+         
+        }
+
+
+    },[statusEventValue]);
+
+    useEffect(()=> {
+        
+        if(lssValue !== null  && statusEventValue[2]=== null){
+            
+            if(lssValue[1] !== null){
+                
+                setStatusColor(lssValue[1]?.color);
+ 
+                // setStatusColor(lssValue[1].color);
+            }
+        }
+    },[lssValue])
 
     // console.log(props.lot.LotID);
     // console.log(props.statusEvents);
@@ -54,7 +85,10 @@ const AccordionEntry: React.FC<any> = (props) => {
 
     return (
         <div className="accordion-item">
-            <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
+            <div className="accordion-title" onClick={() => setIsActive(!isActive)}
+                style={{
+                    backgroundColor: statusColor
+                }}>
                 <>
                     <div>{props.lot.LotName}</div>
                     <div>{isActive ? '-' : '+'}</div>
@@ -67,7 +101,12 @@ const AccordionEntry: React.FC<any> = (props) => {
                 <>
                     <div className="accordion-content">
                         <div><i>{props.lot.LotDescription}</i></div>
-                        <div>Current Status: {statusEventValue[0] ? statusEventValue[0].Description: 'none'}</div>
+                        {/* nested ternary */}
+                        <div>Current Status: {statusEventValue[0] ? statusEventValue[0].Description :
+                             (lssValue[0] ? <i>{lssValue[1].description}</i> : 'none')}
+                             <div className="display_box" 
+                                style={{ backgroundColor: statusColor}}>&nbsp; &nbsp; </div>
+                             </div>
                         
 
                         {props.lot.LotStatusSchedule &&
