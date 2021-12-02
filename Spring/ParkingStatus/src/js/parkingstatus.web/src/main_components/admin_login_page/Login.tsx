9631e838d874052e16c0cef04b95b9b31a63a6a4
dennalig <dennaligrissom7@gmail.com>
2021-12-01 20:51:38 -0500

    start admin login page

--- Spring/ParkingStatus/src/js/parkingstatus.web/src/main_components/admin_login_page/Login.tsx
+++ Spring/ParkingStatus/src/js/parkingstatus.web/src/main_components/admin_login_page/Login.tsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, {useState, useEffect, FormEvent} from 'react'
 import { Link } from 'react-router-dom';
 
 import '../general_style/sign_up_login_style.css';
 
+//services
+import AdminUserService from '../../services/AdminUserService';
+
+type AdminUser ={
+    email: string| null,
+    password: string |null
+}
+
 const SignIn: React.FC = () => {
+    var emailMessage : string = ' does not exist within Parking Status.'; 
+    var emptyEmailMessage : string = 'The email must not be empty.';
+    var pwMessage : string = 'The password entered is incorrect.';
+
+    const[user, setUser] = useState<AdminUser | null>(null);
+
+    const[emptyEmailValue, setEmptyEmailValue] = useState<boolean>(false);
+    const[validEmail, setValidEmail] = useState<boolean>(false);
+    const[validPassword, setValidPassword] = useState<boolean>(false);
+
+
+    const handleEmailNotNull = async (email : string) => {
+
+        email = email.trim();
+        // console.log(email);
+
+        if(email !== ''){
+            setEmptyEmailValue(false);
+            return true;
+
+        }
+        else{
+            setEmptyEmailValue(true);
+            return false;
+        }
+    }
+
+    const handleLoginAttempt = async (event : FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const { email, password } = event.target as typeof event.target & {
+            email : {value : string}
+            password : {value : string}
+        }
+
+        if(await handleEmailNotNull(email.value)){
+            // console.log('here');
+
+            setUser({
+                email : email.value,
+                password : password.value
+            });
+        }
+
+        // console.log(email.value);
+        // console.log(password.value);
+
+
+
+    }
+
     return (
         
         <div>
@@ -11,23 +70,35 @@ const SignIn: React.FC = () => {
                 <h1> Admin Login </h1>
             </div>
 
-            <div className="input_style">
+            <form onSubmit={ event => {handleLoginAttempt(event)}}>
 
-                <label>Email:
-                <input id="email" type="email" 
-                    placeholder="email">
-                </input>
-                </label>
+                    <fieldset className="input_style">
 
-            </div>
+                    <label>Email:
+                    <input id="email" type="email" 
+                        placeholder="email">
+                    </input>
+                    </label>
+                    </fieldset>
 
-            <div className="input_style">
-            <label>Password:
-            <input id="Password" type="password" 
-                    placeholder="password">
-                </input>
-            </label>
-            </div>
+               
+                <fieldset className="input_style">
+
+                
+                    <label>Password:
+                        <input id="password" type="password" 
+                            placeholder="password">
+                        </input>
+                    </label>
+                
+                    </fieldset>
+
+                    <fieldset className="input_style">
+                    <button 
+                        type="submit">Login</button>
+                    </fieldset>
+
+            </form>
             
         </div>
 
